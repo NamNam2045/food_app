fe2de2e5a0d8b9e3499364481bd223fc71b41393
NamNam2045 <hvnsensei@gmail.com>
2026-05-27 09:42:08 +0700

    chore: keep local Android host override (192.168.50.23)

--- food_app/lib/core/constants/app_constants.dart
+++ food_app/lib/core/constants/app_constants.dart
@@ -15,7 +15,7 @@ class AppConstants {
 
     // Android emulator cannot access host via localhost.
     if (defaultTargetPlatform == TargetPlatform.android) {
-      return 'http://10.0.2.2:8080/api/v1';
+      return 'http://192.168.50.23:8080/api/v1';
     }
 
     return 'http://localhost:8080/api/v1';
